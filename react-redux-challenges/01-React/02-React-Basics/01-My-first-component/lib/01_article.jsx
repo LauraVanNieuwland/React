dfd557a857cfc3186f72b876ef8bffccbe1a81e0
Laura van Nieuwland <lauravannieuwland@gmail.com>
2021-02-15 19:39:37 +0100

    Add components with 2 different states title and body

--- react-redux-challenges/01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
+++ react-redux-challenges/01-React/02-React-Basics/01-My-first-component/lib/01_article.jsx
@@ -1,9 +1,22 @@
 // TODO: import the right dependencies
+import React, { Component } from 'react';
 
 // TODO: code the Article component
+class Article extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { title: "My fancy new product", body: "Test it now!"  };
+  }
 
-
-
-
+  render() {
+    return (
+      <div className="article">
+        <h2>{this.title}</h2>
+        <p>{this.body}</p>
+      </div>
+    );
+  }
+}
 
 // TODO: export your Article component for tests
+export default Article
