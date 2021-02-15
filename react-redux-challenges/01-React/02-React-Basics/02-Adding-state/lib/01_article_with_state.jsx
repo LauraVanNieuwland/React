755d0e2b11fb2b1cfdc3bddea4a632297eba28fb
Laura van Nieuwland <lauravannieuwland@gmail.com>
2021-02-15 20:29:25 +0100

    Add state OnClick react

--- react-redux-challenges/01-React/02-React-Basics/02-Adding-state/lib/01_article_with_state.jsx
+++ react-redux-challenges/01-React/02-React-Basics/02-Adding-state/lib/01_article_with_state.jsx
@@ -1,17 +1,29 @@
 // TODO: update dependencies if needed
-import React from 'react';
+import React, { Component } from 'react';
 
 // TODO: transform the following functional component into a class based component
+class Article extends Component {
+  constructor(props) {
+    super(props)
 // TODO: set the initial state { clicked: false }
-// TODO: listen to the click event on `.article`
+    this.state = { clicked: false };
+  }
 // TODO: code the `handleClick` function (which adds a 'clicked' class to the `.article`)
-const Article = (props) => {
-  return (
-    <div className="article">
-      <h2 className="article-title">{props.title}</h2>
-      <p>{props.body}</p>
-    </div>
-  );
+  handleClick = () => {
+    this.setState({ clicked: true });
+  }
+
+  render() {
+    return (
+      <div className={this.state.clicked ? "clicked" : ""}
+          onClick={this.handleClick}>
+        <h2 className="article-title">{this.props.title}</h2>
+        <p>{this.props.body}</p>
+      </div>
+    );
+  }
 }
+
+// TODO: listen to the click event on `.article`
 
 export default Article;
